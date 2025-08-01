b71b25e653e6f0c9bf6edddd4439dbf552eb1c55
anuragg1117 <anuragg117@yahoo.com>
2025-08-01 23:52:08 +0530

    update

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    // 7 days in milliseconds
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // used to prevent attacks as it acceptsonly http requests
+    sameSite: "strict",
+    secure: process.env.NODE_ENV != "development", // this is true only in production
+  });
+  return token;
+};
